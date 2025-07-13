ab365bd26ed4f185752210e1c7c05fb98b7e3234
Abdalla <abdalla2019.1995@gmail.com>
2025-07-13 10:21:39 +0530

    Added tests for updating a recipe.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,10 +3,24 @@ Serializers for recipe APIs.
 """
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+    )
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, required=False)
+
     class Meta:
         model = Recipe
         fields = [
@@ -16,8 +30,23 @@ class RecipeSerializer(serializers.ModelSerializer):
             'price',
             'link',
             'description',
+            'tags',
         ]
         read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
